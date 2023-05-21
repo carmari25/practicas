12a2f9acdf7c2989ddce20c175291389dbe6774a
carmari25 <133187889+carmari25@users.noreply.github.com>
2023-05-21 17:28:01 -0400

    Add files via upload

---
+++ pract1.py
@@ -0,0 +1,7 @@
+variable1= input("Escriba el primer numero:  ")
+variable2=input("Escriba el segundo numero:  ")
+if(variable1>variable2): 
+    print("si aplica que el primer valor ingrsado es mayor que el segundo")
+else:
+    print("no aplica")
+
