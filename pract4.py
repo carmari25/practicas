4474c665041e523799304005a1f69a0caf022221
carmari25 <133187889+carmari25@users.noreply.github.com>
2023-05-22 20:23:18 -0400

    Add files via upload

---
+++ pract4.py
@@ -0,0 +1,8 @@
+#Algoritmo que muestra por pantalla el triple de un numero real introducido por el usuario
+
+print("Si desea saber el triple de un numero introdusca el numero")
+
+var= int(input("\nnumero a triplicar:  "))
+resultado= var * 3
+
+print("\nla respuesta es ", resultado)
