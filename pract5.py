1e476c4f0c87d0f5df9a22695d0a6d88432a35d7
carmari25 <133187889+carmari25@users.noreply.github.com>
2023-05-22 21:00:18 -0400

    Add files via upload

---
+++ pract5.py
@@ -0,0 +1,8 @@
+#Algoritmo pide un numero y escribe su cuadrado
+
+print("Programa para elevar al cuadrado")
+
+var= int(input("introdusca numero: "))
+resultado= var**2
+
+print("\nel resultado es " , resultado)
