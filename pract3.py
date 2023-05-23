5159ce5e208473f5e176ce172790a906c043a373
carmari25 <133187889+carmari25@users.noreply.github.com>
2023-05-22 20:06:52 -0400

    Add files via upload

---
+++ pract3.py
@@ -0,0 +1,9 @@
+#pedir 2 numeros y mostrar la suma de ambos (ejercicio 2)
+
+print ("para sumar los numeros primero ingrese las cantidades")
+
+variable1= int(input("\nintrodusca el primer numero   "))
+variable2= int(input("\nintrodusca el segundo numero  "))
+resultado=variable1 + variable2
+
+print ("\nla suma de ambos es"  , resultado)
