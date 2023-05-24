2faec8ac78747c7890c6e9dcc1446aeb7642a469
carmari25 <133187889+carmari25@users.noreply.github.com>
2023-05-23 21:44:50 -0400

    Add files via upload

---
+++ pract8.py
@@ -0,0 +1,36 @@
+#Pdir un numero al usuario y mostrar el nombre del dia al corresponde(1=lunes)
+
+var1=int(input("ingrese un numero que este entre 1 y 7 para indicar que dia es: "))
+
+if (var1==1):
+    
+    print("lunes")
+ 
+elif (var1==2):
+    
+    print("martes")
+
+elif (var1==3): 
+    
+    print("miercoles")
+
+elif (var1==4):
+
+    print("jueves")
+
+elif (var1==5):
+
+   print("viernes")
+
+elif (var1==6):
+   
+    print("sabado")
+
+elif (var1==7):
+    
+    print("domingo")
+
+
+else: 
+    print("no aplica")
+
