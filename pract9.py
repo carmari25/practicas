de66c69de629f242dc48fe2e88ba165d533807cd
carmari25 <133187889+carmari25@users.noreply.github.com>
2023-05-25 18:53:38 -0400

    Add files via upload

---
+++ pract9.py
@@ -0,0 +1,57 @@
+# ciclo for para los dias de la semana
+
+
+def var():
+
+ cont=0
+ cont=cont+1
+
+
+for i in range(8):
+  if (i==1):
+    
+    print("lunes")
+
+   
+  elif (i==2):
+    
+    print("martes")
+ 
+
+  elif (i==3): 
+    
+    print("miercoles")
+    
+   
+
+  elif (i==4):
+
+    print("jueves")
+
+ 
+
+  elif (i==5):
+
+   print("viernes")
+
+
+
+  elif (i==6):
+   
+    print("sabado")
+
+  
+
+  elif (i==7):
+    
+    print("domingo")
+     
+   
+  break
+     
+   
+
+
+
+
+
