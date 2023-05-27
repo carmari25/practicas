f07f88a699c3ca178a7a13b16c01b779642277f2
carmari25 <133187889+carmari25@users.noreply.github.com>
2023-05-26 23:51:23 -0400

    Add files via upload

---
+++ pract14.py
@@ -0,0 +1,12 @@
+#ALGORITMO N55_Repetir;
+contador=0
+suma=0
+
+while True:
+    
+    print(contador)
+    contador = contador+1
+    suma = suma + contador
+    if contador==5:
+        
+        break
